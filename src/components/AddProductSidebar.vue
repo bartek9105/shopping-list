f8a81a9c44345dac3475f878bd4f151dfc7e8c8b
bartek9105 <bartek9105@gmail.com>
2021-04-13 16:23:57 +0200

    create sidebar for adding new product

---
+++ src/components/AddProductSidebar.vue
@@ -0,0 +1,61 @@
+<template>
+	<v-navigation-drawer
+		v-model="showSidebar"
+		absolute
+		right
+		temporary
+		class="pt-8 px-8"
+		width="600"
+	>
+		<h1>Add new product</h1>
+		<v-form v-model="valid" class="d-flex flex-column">
+			<v-container>
+				<v-row>
+					<v-col cols="12">
+						<v-text-field
+							v-model="product.name"
+							label="Product name"
+							required
+						></v-text-field>
+						<v-text-field
+							v-model="product.description"
+							label="Product description"
+							required
+						></v-text-field>
+					</v-col>
+					<v-select :items="categoriesName" label="Standard"></v-select>
+				</v-row>
+				<v-btn color="success">Add</v-btn>
+			</v-container>
+		</v-form>
+	</v-navigation-drawer>
+</template>
+
+<script>
+export default {
+	name: 'AddProductSidebar',
+	props: {
+		showSidebar: {
+			required: true,
+			type: Boolean
+		},
+		categories: {
+			required: true,
+			type: Array
+		}
+	},
+	data() {
+		return {
+			product: {
+				name: '',
+				description: ''
+			}
+		}
+	},
+	computed: {
+		categoriesName() {
+			return this.categories.map((category) => category.name)
+		}
+	}
+}
+</script>
--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,42 +1,66 @@
 <template>
-	<v-row class="justify-center">
-		<v-col xs="12" md="6">
-			<v-simple-table class="py-6">
-				<template v-slot:default>
-					<thead>
-						<tr>
-							<th class="text-left">
-								Name
-							</th>
-							<th class="text-left">
-								Action
-							</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr v-for="(product, index) in products" :key="index">
-							<td>{{ product.name }}</td>
-							<td>
-								<v-btn small color="primary" dark @click="deleteProduct(index)">
-									Delete product
-								</v-btn>
-							</td>
-						</tr>
-					</tbody>
-				</template>
-			</v-simple-table>
-			<v-snackbar v-model="showSnackbar" timeout="2000">
-				{{ snackbarText }}
-			</v-snackbar>
-		</v-col>
-	</v-row>
+	<div>
+		<v-row class="justify-center">
+			<v-col xs="12" md="6">
+				<v-btn
+					color="success"
+					class="mt-6 d-flex justify-end"
+					@click="showSidebar = true"
+				>
+					Add product
+				</v-btn>
+				<v-progress-circular
+					indeterminate
+					color="primary"
+					v-if="isLoading"
+				></v-progress-circular>
+				<v-simple-table class="py-6" v-else>
+					<template v-slot:default>
+						<thead>
+							<tr>
+								<th class="text-left">
+									Name
+								</th>
+								<th class="text-left">
+									Action
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr v-for="(product, index) in products" :key="index">
+								<td>{{ product.name }}</td>
+								<td>
+									<v-btn
+										small
+										color="primary"
+										dark
+										@click="deleteProduct(index)"
+									>
+										Delete product
+									</v-btn>
+								</td>
+							</tr>
+						</tbody>
+					</template>
+				</v-simple-table>
+				<v-snackbar v-model="showSnackbar" timeout="2000">
+					{{ snackbarText }}
+				</v-snackbar>
+			</v-col>
+		</v-row>
+		<add-product-sidebar :show-sidebar="showSidebar" :categories="categories" />
+	</div>
 </template>
 
 <script>
 import axios from 'axios'
+import AddProductSidebar from '@/components/AddProductSidebar'
 
 export default {
 	name: 'Products',
+	components: {
+		AddProductSidebar
+	},
 	data() {
 		return {
 			product: {
@@ -45,7 +69,10 @@ export default {
 			},
 			products: [],
 			showSnackbar: false,
-			snackbarText: ''
+			snackbarText: '',
+			isLoading: true,
+			showSidebar: false,
+			categories: []
 		}
 	},
 	mounted() {
@@ -53,6 +80,15 @@ export default {
 			.get('https://my.api.mockaroo.com/products.json?key=ff64ad20')
 			.then((response) => {
 				this.products = response.data
+				this.isLoading = false
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+		axios
+			.get('https://my.api.mockaroo.com/categories.json?key=ff64ad20')
+			.then((response) => {
+				this.categories = response.data
 			})
 			.catch((error) => {
 				console.log(error)
