95c9b9b7028ed17b0e3305fa545b6c1d84cb9fb2
bartek9105 <bartek9105@gmail.com>
2021-04-14 14:36:37 +0200

    put navbar to separate component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
 <template>
 	<v-app>
-		<v-app-bar app color="primary" dark>
-			<div class="d-flex align-center">
-				<span class="mr-2">Shopping list app</span>
-				<router-link :to="{ name: 'Categories' }" class="white--text mr-2"
-					>Categories</router-link
-				>
-				<router-link :to="{ name: 'Products' }" class="white--text mr-2"
-					>Products</router-link
-				>
-			</div>
-			<v-spacer></v-spacer>
-		</v-app-bar>
-
+		<app-navbar />
 		<v-main>
 			<router-view />
 		</v-main>
@@ -20,7 +8,12 @@
 </template>
 
 <script>
+import AppNavbar from '@/components/AppNavbar'
+
 export default {
-	name: 'App'
+	name: 'App',
+	components: {
+		AppNavbar
+	}
 }
 </script>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-app-bar app color="primary" dark>
+		<div class="d-flex align-center">
+			<h3 class="mr-8">Shopping list app</h3>
+			<router-link :to="{ name: 'Categories' }" class="white--text mr-4">
+				Categories
+			</router-link>
+			<router-link :to="{ name: 'Products' }" class="white--text mr-4">
+				Products
+			</router-link>
+			<router-link :to="{ name: 'Lists' }" class="white--text mr-4">
+				Shopping lists
+			</router-link>
+		</div>
+		<v-spacer></v-spacer>
+	</v-app-bar>
+</template>
+
+<script>
+export default {
+	name: 'AppNavbar'
+}
+</script>
+
+<style scoped>
+a {
+	text-decoration: none;
+}
+</style>
