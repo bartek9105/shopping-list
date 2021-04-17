6172df2f1809631c91db48aae6f3458b7f63825a
bartek9105 <bartek9105@gmail.com>
2021-04-17 13:15:12 +0200

    change shopping list card color if is completed, display snackbars

--- src/views/Lists.vue
+++ src/views/Lists.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        <v-row class="justify-center mt-4 px-4">
+	<div>
+		<v-row class="justify-center mt-4 px-4">
 			<v-col xs="12" sm="8" md="6">
 				<div class="d-flex justify-space-between align-center">
 					<div class="d-flex align-items">
@@ -12,7 +12,7 @@
 				</div>
 			</v-col>
 		</v-row>
-        <v-row class="justify-center">
+		<v-row class="justify-center">
 			<v-col xs="12" sm="8" md="6">
 				<v-progress-circular
 					indeterminate
@@ -23,98 +23,99 @@
 				<div v-else>
 					<v-simple-table class="py-6" v-if="lists.length > 0">
 						<template v-slot:default>
-							<thead>
-								<tr>
-									<th class="text-left">
-										Name
-									</th>
-								</tr>
-							</thead>
 							<tbody>
 								<tr v-for="(list, listIndex) in lists" :key="listIndex">
-									<v-card shaped tile outlined>
-                                        <v-card-title>
-                                            <v-text-field
-                                                v-model="list.name"
-                                                counter="50"
-                                                hint="Change list name"
-                                                label="List name"
-                                            ></v-text-field>
-                                        </v-card-title>
-                                        <v-card-subtitle>
-                                            ID: {{list.id}}
-                                            Created: {{list.createdAt}}
-                                        </v-card-subtitle>
-                                        <v-card-actions>
-                                            <v-container
-                                                class="px-0"
-                                                fluid
-                                            >
-                                                <v-checkbox
-                                                v-model="list.completed"
-                                                label="Completed"
-                                                ></v-checkbox>
-                                            </v-container>
-                                            <v-btn
-                                                small
-                                                color="error"
-                                                dark
-                                                @click="deleteList(listIndex)"
-                                            >
-                                                DELETE
-                                            </v-btn>
-                                        </v-card-actions>
-                                        <v-expansion-panels>
-                                            <v-expansion-panel>
-                                                <v-expansion-panel-header>
-                                                    Products
-                                                </v-expansion-panel-header>
-                                                <v-expansion-panel-content>
-                                                    <tr v-for="(product, productIndex) in list.products" :key="productIndex">
-                                                        <td>
-                                                            {{ product.name }}
-                                                            <v-chip class="ma-2" color="success" outlined>
-                                                                {{ product.category }}
-                                                            </v-chip>
-                                                        </td>
-                                                        <td>
-                                                            <v-btn
-                                                                small
-                                                                color="primary"
-                                                                dark
-                                                                @click="deleteProductFromList(list, productIndex)"
-                                                            >
-                                                                Delete
-                                                            </v-btn>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <td>
-                                                            <v-select 
-                                                                :items="products" 
-                                                                label="Select product"
-                                                                item-text="name"
-                                                                v-model="selectedProduct"
-                                                                return-object
-                                                                >
-                                                            </v-select>
-                                                        </td>
-                                                        <td>
-                                                            <v-btn
-                                                                small
-                                                                color="success"
-                                                                dark
-                                                                @click="addProductToList(list)"
-                                                            >
-                                                                Add product
-                                                            </v-btn>
-                                                        </td>
-                                                    </tr>
-                                                </v-expansion-panel-content>
-                                            </v-expansion-panel>
-                                        </v-expansion-panels>
-                                        
-                                    </v-card>
+									<v-card
+										shaped
+										tile
+										outlined
+										class="mb-8"
+										:class="{ 'green lighten-5': list.completed }"
+									>
+										<v-card-title>
+											<v-text-field
+												v-model="list.name"
+												counter="50"
+												hint="Change list name"
+												label="List name"
+											></v-text-field>
+										</v-card-title>
+										<v-card-subtitle>
+											Created:
+											{{ new Date(Date.parse(list.createdAt)).toDateString() }}
+										</v-card-subtitle>
+										<v-card-actions>
+											<v-container class="px-0" fluid>
+												<v-checkbox
+													v-model="list.completed"
+													label="Completed"
+													@click="completeList"
+												></v-checkbox>
+											</v-container>
+											<v-btn
+												small
+												color="error"
+												dark
+												@click="deleteList(listIndex)"
+											>
+												DELETE
+											</v-btn>
+										</v-card-actions>
+										<v-expansion-panels>
+											<v-expansion-panel>
+												<v-expansion-panel-header>
+													Products
+												</v-expansion-panel-header>
+												<v-expansion-panel-content>
+													<tr
+														v-for="(product, productIndex) in list.products"
+														:key="productIndex"
+													>
+														<td>
+															{{ product.name }}
+															<v-chip class="ma-2" color="success" outlined>
+																{{ JSON.parse(product.category)[0].name }}
+															</v-chip>
+														</td>
+														<td>
+															<v-btn
+																small
+																color="primary"
+																dark
+																@click="
+																	deleteProductFromList(list, productIndex)
+																"
+															>
+																Delete
+															</v-btn>
+														</td>
+													</tr>
+													<tr>
+														<td>
+															<v-select
+																:items="products"
+																label="Select product"
+																item-text="name"
+																v-model="selectedProduct"
+																return-object
+															>
+															</v-select>
+														</td>
+														<td>
+															<v-btn
+																small
+																color="success"
+																dark
+																@click="addProductToList(list)"
+															>
+																Add product
+															</v-btn>
+														</td>
+													</tr>
+												</v-expansion-panel-content>
+											</v-expansion-panel>
+										</v-expansion-panels>
+									</v-card>
 								</tr>
 							</tbody>
 						</template>
@@ -125,46 +126,57 @@
 				</div>
 			</v-col>
 		</v-row>
-    </div>
+		<v-snackbar v-model="showSnackbar" timeout="2000">
+			{{ snackbarText }}
+		</v-snackbar>
+	</div>
 </template>
 
 <script>
-    import axios from 'axios'
+import axios from 'axios'
 
-    export default {
-        name: 'Lists',
-        data() {
-            return {
-                lists: [],
-                products: [],
-                selectedProduct: {},
-                isLoading: true
-            }
-        },
-        methods:{
-            addList(){
-                this.lists.push(
-                    {
-                        name:"test", 
-                        id: 999, 
-                        createdAt:"just now", 
-                        completed: false, 
-                        products: []
-                    }
-                )
-            },
-            deleteList(index){
-                this.lists.splice(index, 1);
-            },
-            addProductToList(list){
-                list.products.push(this.selectedProduct);
-            },
-            deleteProductFromList(list, index){
-                list.products.splice(index,1);
-            }
-        },
-        mounted() {
-        axios
+export default {
+	name: 'Lists',
+	data() {
+		return {
+			lists: [],
+			products: [],
+			selectedProduct: {},
+			isLoading: true,
+			showSnackbar: false,
+			snackbarText: ''
+		}
+	},
+	methods: {
+		addList() {
+			this.lists.unshift({
+				name: '',
+				id: Date.now(),
+				createdAt: new Date().toDateString(),
+				completed: false,
+				products: []
+			})
+			this.snackbarText = 'New shopping list added'
+			this.showSnackbar = true
+		},
+		deleteList(index) {
+			this.lists.splice(index, 1)
+			this.snackbarText = 'Shopping list deleted'
+			this.showSnackbar = true
+		},
+		addProductToList(list) {
+			list.products.push(this.selectedProduct)
+		},
+		deleteProductFromList(list, index) {
+			list.products.splice(index, 1)
+		},
+		completeList() {
+			this.snackbarText = 'List marked as completed'
+			this.showSnackbar = true
+		}
+	},
+	mounted() {
+		axios
 			.get('products.json')
 			.then((response) => {
 				this.products = response.data
@@ -174,7 +186,7 @@
 				console.log(error)
 			})
 		axios
-			.get('lists.json')
+			.get('https://my.api.mockaroo.com/lists.json?key=ff64ad20')
 			.then((response) => {
 				this.lists = response.data
 				this.isLoading = false
@@ -182,6 +194,6 @@
 			.catch((error) => {
 				console.log(error)
 			})
-        },        
-    }
+	}
+}
 </script>
